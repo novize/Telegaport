86eb1d7688ceb1f0d8710cd4c58dd92805ff052a
Сергей Резник <zamery14@yandex.ru>
2024-07-30 16:24:25 +0300

    9.15. Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import './full-size-image.js';
 import './form-validation.js';
+import './scale.js';
+import './effects.js';
 import { postsGenerator } from './posts.js';
 import { generateThumbnails } from './thumbnails.js';
 
