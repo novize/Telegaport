9578f0057ce57c74cca1e8011da01fdb4b5075a7
Сергей Резник <zamery14@yandex.ru>
2024-11-24 19:14:58 +0300

    Чистка файлов

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 import './scale.js';
-import './form-validation.js';
-import { init } from './effects.js';
 import { getData } from './api.js';
 import { renderPictures } from './gallery.js';
 import { showAlert } from './util.js';
-import { initForm } from './form.js';
 
 (() => {
   getData()
